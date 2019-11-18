f3c5f27465aecd212243949891c931ac600ae6d8
Lucy Follansbee <SVC_DEV_CF>
2019-11-18 12:09:05 -0700

    update top nav and update prettier formatting to match eslint better

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,21 @@
 <template>
-  <v-app>
+  <v-app >
     <v-app-bar
       app
       color="primary"
       dark
     >
-      <div class="d-flex align-center">
-        <v-img
-          alt="Vuetify Logo"
-          class="shrink mr-2"
-          contain
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
-          transition="scale-transition"
-          width="40"
-        />
+      <router-link to="/"><v-toolbar-title>DGC Ping Pong</v-toolbar-title></router-link>
 
-        <v-img
-          alt="Vuetify Name"
-          class="shrink mt-1 hidden-sm-and-down"
-          contain
-          min-width="100"
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png"
-          width="100"
-        />
-      </div>
+      <v-spacer></v-spacer>
+
+      <v-btn icon>
+        <v-icon>mdi-heart</v-icon>
+      </v-btn>
+
+      <v-btn icon>
+        <v-icon>mdi-magnify</v-icon>
+      </v-btn>
     </v-app-bar>
 
     <v-content>
@@ -41,3 +33,10 @@ export default {
   data: () => ({}),
 };
 </script>
+
+<style lang="scss">
+.v-toolbar__title {
+  color: white;
+  font-weight: bold;
+}
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -14,7 +14,7 @@ const routes = [
   {
     component: Player,
     name: 'player',
-    path: '/:playerId',
+    path: '/player/:playerId',
   },
 ];
 
